63548ebb9dc06706073bf39cbddbe81fc00adc36
Ilian Nilsson <ilian.nilsson@live.se>
2026-08-27 09:17:33 +0200

    Name the crate after its repository; take what this module owns

---
+++ target/debug/deps/xmip_core_authenticate-63c6190be470cc12.d
@@ -0,0 +1,5 @@
+D:\Repos\Xmip\modules\capabilities\authenticate\target\debug\deps\xmip_core_authenticate-63c6190be470cc12.d: src\lib.rs
+
+D:\Repos\Xmip\modules\capabilities\authenticate\target\debug\deps\xmip_core_authenticate-63c6190be470cc12.exe: src\lib.rs
+
+src\lib.rs:
---
+++ target/debug/deps/xmip_core_authenticate-c6c2393e3a11b30c.d
@@ -0,0 +1,7 @@
+D:\Repos\Xmip\modules\capabilities\authenticate\target\debug\deps\xmip_core_authenticate-c6c2393e3a11b30c.d: src\lib.rs
+
+D:\Repos\Xmip\modules\capabilities\authenticate\target\debug\deps\libxmip_core_authenticate-c6c2393e3a11b30c.rlib: src\lib.rs
+
+D:\Repos\Xmip\modules\capabilities\authenticate\target\debug\deps\libxmip_core_authenticate-c6c2393e3a11b30c.rmeta: src\lib.rs
+
+src\lib.rs:
